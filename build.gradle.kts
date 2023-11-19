0086731ce0ab489bb5062d50a5f123a7107c0e46
Ainin24 <130471690+Ainin24@users.noreply.github.com>
2023-11-19 20:46:50 +0800

    Lab 3_Task 2(Lemonade)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.diceroller"
+    namespace = "com.example.lemonade"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.diceroller"
+        applicationId = "com.example.lemonade"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
